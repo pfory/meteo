42a4f6135cae2b1f6f5feeb89e6d41a628e0120a
pfory <hanka12>
2019-01-04 18:32:03 +0100

    ota

--- ota.sh
+++ ota.sh
@@ -1,5 +1,5 @@
-nahrát bin do /home/pi
+﻿nahrát bin do /home/pi
 upravit verzi XXXX
 spustit v příkazové řádce
 
-python /home/$USER/.arduino15/packages/esp8266/hardware/esp8266/2.4.1/tools/espota.py -i 192.168.1.102 -p 8266 --auth=password -f /home/pi/MeteoXXXX.ino.bin 
+python /home/$USER/.arduino15/packages/esp8266/hardware/esp8266/2.4.2/tools/espota.py -i 192.168.1.102 -p 8266 --auth=password -f /home/pi/MeteoXXXX.ino.bin 
