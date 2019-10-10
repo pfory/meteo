275fb97c9a1ecf991d6e0ba0d1da9636a8819fed
Petr <pfory@seznam.cz>
2019-10-10 15:16:10 +0200

    1.97

--- Configuration.h
+++ Configuration.h
@@ -2,7 +2,7 @@
 #define CONFIGURATION_H
 
 //SW name & version
-#define     VERSION                          "1.96"
+#define     VERSION                          "1.97"
 #define     SW_NAME                          "METEO"
 
 #define timers
